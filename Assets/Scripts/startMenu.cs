5768862dd4678b3750e3bb4a8aa7f1eab4a4a3ae
skovsmose <mskovsmose@gmail.com>
2016-03-24 12:44:27 +0100

    Added MainMenu start scene

---
+++ Assets/Scripts/startMenu.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class startMenu : MonoBehaviour {
+
+    public Canvas exitMenu;
+    public Button Achievements;
+    public Button exitText;
+
+    // Use this for initialization
+    void Start()
+    {
+        exitMenu = exitMenu.GetComponent<Canvas>();
+        Achievements = Achievements.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        exitMenu.enabled = false;
+    }
+
+    public void ExitPress()
+    {
+        exitMenu.enabled = true;
+        Achievements.enabled = false;
+        exitText.enabled = false;
+
+    }
+
+    public void NoPress()
+    {
+        exitMenu.enabled = false;
+        Achievements.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void GoToAchievements()
+    {
+        Application.LoadLevel("Achievements");
+    }
+    public void GoToLearnCPR()
+    {
+        Application.LoadLevel("LearnCPR");
+    }
+    public void Back()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
