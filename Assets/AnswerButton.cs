6f1736932380f4496925f10f00debc4a7c707cf3
Nucleuz <h-hansen1990@hotmail.com>
2016-04-07 23:47:02 +0200

    Added a Data script for variables such as timesCorrect, timesAnswered, etc. Added a AnswerButton script which still needs some work...

---
+++ Assets/AnswerButton.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class AnswerButton : MonoBehaviour, IPointerDownHandler
+{
+    //public class Answer
+    //{
+    //    public string Name { get; set; }
+    //    public bool IsCorrect { get; set; }
+
+    //}
+
+    //Answer a1_a;
+    //Answer a1_b;
+    //Answer a1_c;
+    //Answer a1_d;
+
+    public bool isCorrectAnswer;
+
+    bool buttonPressed = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        //a1_a = new Answer { Name = "RUN!", IsCorrect = true };
+        //a1_b = new Answer { Name = "Call 777", IsCorrect = false };
+        //a1_c = new Answer { Name = "Nothing", IsCorrect = false };
+        //a1_d = new Answer { Name = "Something completely stupid", IsCorrect = false };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        if (this.isCorrectAnswer == true)
+        {
+            Data.timesAnswered += 1;
+            Data.timesCorrect += 1;
+            Debug.Log("The user has answered: " + Data.timesAnswered + " times.");
+            Debug.Log("Is correct answer! \n Change scene plz.");
+            this.isCorrectAnswer = false;
+        }
+        else
+        {
+            Data.timesAnswered += 1;
+            Debug.Log("Restart scene and let user try again");
+            Debug.Log("The user has answered: " + Data.timesAnswered + "times.");
+        }
+    }
+}
---
+++ Assets/Data.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Data : MonoBehaviour {
+
+    public static int timesAnswered { get; internal set; }
+    public static int timesCorrect { get; internal set; }
+    public static int timesPressed { get; internal set; }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
