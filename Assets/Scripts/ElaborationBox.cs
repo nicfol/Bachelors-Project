ac25957040b3069c19f3dec8796da2caee13e63e
nicolai <nicfol@users.noreply.github.com>
2016-04-07 22:38:37 +0200

    I quit... Need more tutorials on Unity 5 GUI :(

---
+++ Assets/Scripts/ElaborationBox.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElaborationBox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void OnMouseDown() {
+        Debug.Log("Option 1 chosen");
+    }
+}
