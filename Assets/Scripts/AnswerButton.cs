7b3eb44c4685575062e2db519f0f25878c48d629
Nucleuz <h-hansen1990@hotmail.com>
2016-04-13 14:33:13 +0200

    Reworked AnsweringSystem

---
+++ Assets/Scripts/AnswerButton.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour {
+
+    public Text question;
+
+    public GameObject correctAnswerWindow;
+    public GameObject wrongAnswerWindow;
+
+    public bool windowActive = false;
+
+    public string nextScene;
+
+    public void CorrectAnswer()
+    {
+        // Checks if window
+        if (correctAnswerWindow.activeSelf == false & windowActive == false)
+        {
+            // Show window to proceed to next question/challenge
+            correctAnswerWindow.SetActive(true);
+            windowActive = true;
+            // Increment times answered and correct
+            Data.timesAnswered += 1;
+            Data.timesCorrect += 1;
+            
+            // Add correct answer to the List of correct answers
+            Data.correctAnswers.Add(question.text);
+        }
+    }
+
+    public void WrongAnswer()
+    {
+        if (wrongAnswerWindow.activeSelf == false & windowActive == false)
+        {
+            // Show wrong window - try again
+            wrongAnswerWindow.SetActive(true);
+            windowActive = true;
+            // Increment times answered
+            Data.timesAnswered += 1;
+
+            // Add wrong answer to List of wrong answers
+            Data.wrongAnswers.Add(question.text);
+        }
+    }
+
+    public void NextScene()
+    {
+        // Change to next scene. Please write the scene name in the Inspector.
+        SceneManager.LoadScene(nextScene);
+    }
+
+
+    // Close wrong answer window
+    public void CloseWrongAnswerWindow()
+    {
+        wrongAnswerWindow.SetActive(false);
+        windowActive = false;
+    }
+}
---
+++ Assets/Strings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Strings : MonoBehaviour {
+
+    private string s1s1a1 = "correct";
+    private string s1s1a2 = "wrong";
+
+    public string S1s1a1
+    {
+        get
+        {
+            return s1s1a1;
+        }
+    }
+}
