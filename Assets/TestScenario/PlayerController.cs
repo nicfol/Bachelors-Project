cad8c187e836a98d5dae00e1e3571ddccb57c807
nicolai <nicfol@users.noreply.github.com>
2016-04-14 17:18:19 +0200

    Moved script from player to a handler object.. should probably have committed a while ago :

--- Assets/TestScenario/PlayerController.cs
+++ Assets/TestScenario/PlayerController.cs
@@ -3,45 +3,55 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
     
+    public GameObject player;
+    public Camera cam;
+    
     public Transform[] playerTargets;
     public Transform[] cameraTargets;
-    private int question = 0;
+    public float[] camZoom;
     
+    private int question = 0;
+
     private float transitionDuration = 2.5f;
 
 	// Use this for initialization
 	void Start () {
+        if(cam == null) {
+            cam = Camera.main;
+        }
+        if (player == null) {
+            player = GameObject.Find("Player");
+        }
+        
 	    StartCoroutine("movePlayer");
 	}
 	
 	// Update is called once per frame
 	void Update () {        
         Debug.Log(question + " " + transitionDuration);
-                
-        if (question == 1) {
-            transitionDuration = 1.0f;
-        }
             
         if(Input.GetKeyDown("space")) {
             StartCoroutine("movePlayer");
         }
 	}
     
-      
     IEnumerator movePlayer() {
-            
         float t = 0.0f;
-        Vector3 startingPos = transform.position;
-        Vector3 camStartingPos = Camera.main.transform.position;
+        Vector3 startingPos = player.transform.position;
+        Vector3 camStartingPos = cam.transform.position;
         while (t < 1.0f) {
             t += Time.deltaTime * (Time.timeScale/transitionDuration);
-            transform.position = Vector3.Lerp(startingPos, playerTargets[question].position, t);
             
-            if(question == 0) {
-                Camera.main.transform.position = Vector3.Lerp(camStartingPos, cameraTargets[question].position, t);
-            } else if (question == 1) {
-                Camera.main.transform.position = Vector3.Lerp(camStartingPos, cameraTargets[question].position, t);
-                Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, 2, t/20);
+            if(playerTargets[question] != null) {
+                player.transform.position = Vector3.Lerp(startingPos, playerTargets[question].position, t);
+            }
+            
+            if(cameraTargets[question] != null) {
+                cam.transform.position = Vector3.Lerp(camStartingPos, cameraTargets[question].position, t);
+                
+                if(camZoom[question] != null && cam.orthographicSize != camZoom[question]) {
+                    cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, camZoom[question], t/20);
+                }                
             }
             yield return null;
         }
