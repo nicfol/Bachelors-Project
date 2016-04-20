84fd2b9385a4cc6f00c1fa1d7a030b09dd3bc155
Nucleuz <h-hansen1990@hotmail.com>
2016-04-20 23:51:10 +0200

    Not done at all...

---
+++ Assets/Scripts/Heartbeat.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Heartbeat : MonoBehaviour {
+
+    private bool hasTapped = false;
+    private float timePassed;
+    private bool start = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            start = true;
+            timePassed = 0;
+        }
+
+        if (start)
+        {
+            if (timePassed <= 30)
+            {
+                timePassed += Time.deltaTime;
+            }
+
+            Debug.Log(timePassed);
+
+
+
+        }    
+	}
+
+    public void TapHeart()
+    {
+        hasTapped = true;
+    }
+}
