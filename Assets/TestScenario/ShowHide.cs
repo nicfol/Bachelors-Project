426dac3c66af0534bd30aeec70d43f4e87b7e356
nicolai <nicfol@users.noreply.github.com>
2016-04-20 22:50:55 +0200

    Made correct answer button and duplicated 3 times, still need to setup wrong answers

---
+++ Assets/TestScenario/ShowHide.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowHide : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    public void showhideGO() {
+    
+    if(gameObject.activeSelf == true) {
+        
+        gameObject.SetActive(false);
+        
+    } else if(gameObject.activeSelf == false) {
+        
+        gameObject.SetActive(true);
+        
+    }
+    
+}
+}
