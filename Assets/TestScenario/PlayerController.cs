25b2ef98c5fcc4f1ffb6ee411de12d220c37b5a1
nicolai <nicfol@users.noreply.github.com>
2016-04-21 13:00:47 +0200

    Added wrong and correct answer counter and fixed a bunch of errors

--- Assets/TestScenario/PlayerController.cs
+++ Assets/TestScenario/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour {
     private int question = 0;
     private int noOfQuestions;
     
+    private int correctAnswers;
+    private int wrongAnswers;
+    
     public Transform[] playerTargets;
     public Transform[] cameraTargets;
     public float[] camZoom;
@@ -16,7 +19,7 @@ public class PlayerController : MonoBehaviour {
     public GameObject[] questionObjects;
 
     private float transitionDuration = 2.5f;
-
+    
 	void Start () {
         getNumberOfQuestions();
         
@@ -27,12 +30,19 @@ public class PlayerController : MonoBehaviour {
             player = GameObject.Find("Player");
         }
         
+        // ------- HACK ------- //
+            // SHOW ERROR IF playerTargets[], cameraTargets[] or camZoom[] LENGTH DOES NOT 
+            // EQUAL THE AMOUNT OF QUESTIONS IN noOfQuestions
+            if (playerTargets.Length != noOfQuestions || cameraTargets.Length != noOfQuestions || camZoom.Length != noOfQuestions) {
+                Debug.Log("playerTargets[], cameraTargets[] or camZoom[] LENGTH IS NOT THE SAME AS noOfQuestions"); 
+                playerTargets[40] = playerTargets[41];
+            }
+        // ------- HACK ------- //
+        
 	    startMovePlayer();
 	}
 	
 	void Update () {        
-        Debug.Log(question + " " + transitionDuration);
-            
         if(Input.GetKeyDown("space")) {
             startMovePlayer();
         }
@@ -44,41 +54,50 @@ public class PlayerController : MonoBehaviour {
         Debug.Log("No of questions: " + noOfQuestions);
     }
     
+    public void addToWrongAnswer() {
+        wrongAnswers++;
+    }
+    
+    public void addToCorrectAnswer() {
+        correctAnswers++;
+    }
+    
     public void startMovePlayer() {
-        if(question != 0) {
-            questionObjects[question].gameObject.SetActive(false);
-        }
+        Debug.Log(question);
         StartCoroutine("movePlayer");
     }
     
-    IEnumerator movePlayer() {
-                
+    IEnumerator movePlayer() {  
         //Change camera movement speed after intro scene
         if(question == 1.0f) {
             transitionDuration = 1.0f;
         }
+        
+        if (question == noOfQuestions) {
+            Debug.Log("W: " + wrongAnswers + " | C: " + correctAnswers);
+        } else if(question <= noOfQuestions) {        
+            float t = 0.0f;
+            Vector3 startingPos = player.transform.position;
+            Vector3 camStartingPos = cam.transform.position;
+            while (t < 1.0f) {
+                t += Time.deltaTime * (Time.timeScale/transitionDuration);
                 
-        float t = 0.0f;
-        Vector3 startingPos = player.transform.position;
-        Vector3 camStartingPos = cam.transform.position;
-        while (t < 1.0f) {
-            t += Time.deltaTime * (Time.timeScale/transitionDuration);
-            
-            if(playerTargets[question] != null) {
-                player.transform.position = Vector3.Lerp(startingPos, playerTargets[question].position, t);
-            }
-            
-            if(cameraTargets[question] != null) {
-                cam.transform.position = Vector3.Lerp(camStartingPos, cameraTargets[question].position, t);
+                if(playerTargets[question] != null) {
+                    player.transform.position = Vector3.Lerp(startingPos, playerTargets[question].position, t);
+                }
                 
-                if(camZoom[question] != null && cam.orthographicSize != camZoom[question]) {
-                    cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, camZoom[question], t/20);
-                }                
+                if(cameraTargets[question] != null) {
+                    cam.transform.position = Vector3.Lerp(camStartingPos, cameraTargets[question].position, t);
+                    
+                    if(camZoom[question] != null && cam.orthographicSize != camZoom[question]) {
+                        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, camZoom[question], t/20);
+                    }                
+                }
+                yield return null;
             }
-            yield return null;
+            questionObjects[question].gameObject.SetActive(true);
+            question += 1;
         }
-        questionObjects[question].gameObject.SetActive(true);
-        question += 1;
     }
     
 }
