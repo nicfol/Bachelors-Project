5c8fb574da2d0ddeb6da801eab80fc21bc85d6a1
Nucleuz <h-hansen1990@hotmail.com>
2016-04-25 16:38:33 +0200

    Changed the ButtonRed image
    Changed how we save answers

--- Assets/Scripts/EndScenario.cs
+++ Assets/Scripts/EndScenario.cs
@@ -3,77 +3,68 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class EndScenario : MonoBehaviour {
+
     public int numberOfQuestions = 14;
     public int threeStarsMin = 12;
     public int twoStarsMin = 9;
     public int oneStarMin = 7;
-    string timesAnswered;
-    string timesCorrect;
-    string timesWrong;
+
     public Text currentText;
 
-    public GameObject star1;
-    public GameObject star2;
-    public GameObject star3;
-    
+    public bool scenario_1;
+    public bool scenario_2;
+
+    private Scenario scenario;
+
     // Use this for initialization
     void Start () {
-        
-        if(currentText == null)
+
+        if (scenario_1)
+        {
+            scenario = Data.scenario_1;
+        }
+        else if (scenario_2)
+        {
+            scenario = Data.scenario_2;
+        }
+        else
+        {
+            Debug.Log("Stars script: You forgot to set the ID of the scenario.");
+        }
+
+
+        if (currentText == null)
         {
             currentText = GameObject.Find("ScoreText").GetComponent<Text>();
         }
-
-        if(star1 == null)
-        {
-            star1 = GameObject.Find("Star1");
-        }
-        if (star2 == null)
-        {
-            star2 = GameObject.Find("Star2");
-        }
-        if (star3 == null)
-        {
-            star3 = GameObject.Find("Star3");
-        }
-
-        star1.SetActive(false);
-        star2.SetActive(false);
-        star3.SetActive(false);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        timesAnswered = Data.timesAnswered.ToString();
-        timesCorrect = Data.timesCorrect.ToString();
-        timesWrong = Data.wrongAnswers.Count.ToString();
 
-        currentText.text = "You used "+ timesAnswered + " attempts.\nYou got " + timesCorrect + " correct answers.\n You got " + timesWrong + " wrong answers.";
 
-        if (Data.timesAnswered >= threeStarsMin)
+        currentText.text = "You used "+ scenario.timesAnswered.ToString() + " attempts.\nYou got " + scenario.timesCorrect.ToString() + " correct answers.\n You got " + scenario.wrongAnswers.Count.ToString() + " wrong answers.";
+
+        if (scenario.timesCorrect >= threeStarsMin)
         {
             // Show 3 stars
-            star1.SetActive(true);
-            star2.SetActive(true);
-            star3.SetActive(true);
+            scenario.Stars = 3;
+
         }
-        else if (Data.timesAnswered >= twoStarsMin)
+        else if (scenario.timesCorrect >= twoStarsMin)
         {
             // Show 2 stars
-            star1.SetActive(true);
-            star2.SetActive(true);
+            scenario.Stars = 2;
+
         }
-        else if(Data.timesAnswered >= oneStarMin)
+        else if(scenario.timesCorrect >= oneStarMin)
         {
             // Show 1 star  
-            Debug.Log("Star 1 is active!");
-            star1.SetActive(true);         
+            scenario.Stars = 1;
         }
         else
         {
-            star1.SetActive(false);
-            star2.SetActive(false);
-            star3.SetActive(false);
+            scenario.Stars = 0;
         }
 
         
---
+++ Assets/Scripts/Stars.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stars : MonoBehaviour {
+
+    public GameObject star1;
+    public GameObject star2;
+    public GameObject star3;
+
+    public bool scenario_1 = false;
+    public bool scenario_2 = false;
+
+    private Scenario s;
+
+	// Use this for initialization
+	void Start () {
+        s = new Scenario();
+        if (scenario_1)
+        {
+            s = Data.scenario_1;
+        } else if (scenario_2)
+        {
+            s = Data.scenario_2;
+        }
+        else
+        {
+            Debug.Log("Stars script: You forgot to select scenario.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (s.Stars == 3)
+        {
+            // Show 3 stars
+            star1.SetActive(true);
+            star2.SetActive(true);
+            star3.SetActive(true);
+        }
+        else if (s.Stars == 2)
+        {
+            // Show 2 stars
+            star1.SetActive(true);
+            star2.SetActive(true);
+            star3.SetActive(false);
+        }
+        else if (s.Stars == 1)
+        {
+            // Show 1 star  
+            star1.SetActive(true);
+            star2.SetActive(false);
+            star3.SetActive(false);
+        }
+        else
+        {
+            // Show no star
+            star1.SetActive(false);
+            star2.SetActive(false);
+            star3.SetActive(false);
+        }
+    }
+}
