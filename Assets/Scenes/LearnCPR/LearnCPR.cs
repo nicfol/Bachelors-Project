a24569fa151e5c7ebad82a425dca55a9f7f9d2f2
mskovsmose <mskovsmose@gmail.com>
2016-04-25 10:15:22 +0200

    Learn CPR scene fixed

---
+++ Assets/Scenes/LearnCPR/LearnCPR.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LearnCPR : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void BasicCPR()
+    {
+        SceneManager.LoadScene("BasicCPR");
+    }
+
+    public void AED()
+    {
+        SceneManager.LoadScene("AED");
+    }
+
+    public void RecoveryPos()
+    {
+        SceneManager.LoadScene("RecoveryPos");
+    }
+
+    public void BackToMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void BackToLearnCPR()
+    {
+        SceneManager.LoadScene("LearnCPR");
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
