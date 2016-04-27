ef48a4bf76b60ad6ba6881d01da4684a275f542b
nicolai <nicfol@users.noreply.github.com>
2016-04-27 14:43:13 +0200

    Trying to implement back button on android, still not quite

---
+++ Assets/Scripts/HardwareButtonHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class HardwareButtonHandler : MonoBehaviour {
+
+    private string previousScene;
+    private string currentScene;
+    
+	// Use this for initialization
+	void Start () {
+        previousScene = PlayerPrefs.GetString("CurrentScene"); 
+	    PlayerPrefs.SetString("PrevScene", previousScene);
+        
+        currentScene = SceneManager.GetActiveScene().name;
+        Debug.Log(currentScene);
+        PlayerPrefs.SetString("CurrentScene", currentScene);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Escape)) {
+            Debug.Log("HEJ");
+            SceneManager.LoadScene(previousScene);
+        }
+	}
+}
