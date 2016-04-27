7d676b6855716bfe73bc2ec29f80d26af14c2958
Nucleuz <h-hansen1990@hotmail.com>
2016-04-27 10:55:07 +0200

    Added a Lock image Added locked challenges to the challenge scene Added some prefabs for challenges

---
+++ Assets/Scripts/Challenge.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Challenge : MonoBehaviour {
+
+    public GameObject previousChallenge;
+    private GameObject overlay;
+    
+    void Start()
+    {
+        previousChallenge.GetComponent<Stars>();
+        overlay = this.gameObject.transform.FindChild("Lock_Overlay").gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(previousChallenge.GetComponent<Stars>().s.Stars >= 2)
+        {
+            overlay.SetActive(false);
+        }
+        else
+        {
+            overlay.SetActive(true);
+        }
+	}
+}
