2df25399668d32d61de29fb771b1534e96d3affa
Nucleuz <h-hansen1990@hotmail.com>
2016-05-02 16:35:38 +0200

    Added Progression scripts Added a ProgressBar placeholder

---
+++ Assets/Scripts/Progression/Progress.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Progress : MonoBehaviour {
+
+    Image foregroundImage;
+    public Text percentage;
+
+    int value
+    {
+        get
+        {
+            if (foregroundImage != null)
+                return (int)(foregroundImage.fillAmount * 100);
+            else
+                return 0;
+        }
+
+        set
+        {
+            if (foregroundImage != null)
+                foregroundImage.fillAmount = value / 100f;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        foregroundImage = gameObject.GetComponent<Image>();
+        percentage = percentage.GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        percentage.text = value.ToString() + "%";
+        value = Data.Progression;
+    }
+}
